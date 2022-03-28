62a20aead9c282672997cabd5afdc21da49ce950
lucasseet <79414028+lucasseet@users.noreply.github.com>
2022-03-28 20:00:30 +0800

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -38,8 +38,17 @@ namespace API
 
             services.AddDbContext<DataContext>(options =>
             options.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+
+            services.AddCors(opt => {
+
+                opt.AddPolicy("CorsPolicy", policy => 
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
             
         }
+        
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -54,6 +63,8 @@ namespace API
             // app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
